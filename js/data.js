57f17d82f3bbf5ba5944929b0b92a7113b9442db
syugo0101 <mamo0101ru@gmail.com>
2025-05-14 12:02:51 +0900

    各データを対応

--- js/data.js
+++ js/data.js
@@ -2,14 +2,29 @@ document.addEventListener("DOMContentLoaded", () => {
   const params = new URLSearchParams(window.location.search);
   const type = params.get("type");
 
-  const typeMap = {
-    sample:"サンプル",
+    const typeMap = {
+    sample: "サンプル",
     steel: "製鉄所",
     oil: "製油所",
     ethylene: "エチレンプラント",
     aluminum: "アルミニウム製錬所",
-    // 必要に応じて追加
-  };
+    nonferrous: "その他非鉄金属製錬所",
+    cement: "セメント工場",
+    paper: "製紙工場",
+    auto: "自動車組立工場",
+    ship: "造船所",
+    rail: "鉄道車両工場",
+    engine: "エンジン·タービン組立工場",
+    homegas: "電気·ガス·石油機器工場(家庭用)",
+    progas: "電気·ガス·石油機器工場(業務用)",
+    heavyel: "電機工場(重電)",
+    elevator: "電機工場(昇降機)",
+    audio: "電機工場(音響)",
+    semi: "半導体工場(昭和63年時点)",
+    airplane: "飛行機組立工場(昭和17年時点)",
+    arsenal: "陸軍造兵廠·海軍工廠(昭和17年時点)",
+    alumina: "アルミナ製錬と車扱貨物"
+    };
 
   const displayName = typeMap[type] || "不明な分類";
   const csvFile = `data/${type}.csv`;
